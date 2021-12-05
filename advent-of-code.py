07a8fac5f197a4b46557d6e3cae99f8f35311e1f
Hazem Ash <zashm@pm.me>
2021-12-05 01:43:45 +0000

    Solve day-4 par-1

--- advent-of-code.py
+++ advent-of-code.py
@@ -1,41 +1,85 @@
+import argparse
 import numpy as np
+
 from functools import reduce
 
-p_input = [
-"00100",
-"11110",
-"10110",
-"10111",
-"10101",
-"01111",
-"00111",
-"11100",
-"10000",
-"11001",
-"00010",
-"01010",
-]
+parser = argparse.ArgumentParser(description='Solve advent-of-code-2021 day#4 part#1')
+parser.add_argument('-input', '-i', metavar='input_file', type=str, required=True,
+                    help='file with puzzle input')
 
-vectorized_invert = np.vectorize(lambda x : (x ^ 1))
+args = parser.parse_args()
+print(args.input)
 
-all_numbers = []
-with open("input.txt", "r") as p_input:
+########################### Solution ############################
+all_winning_pos = []
+for x in range(5):
+	#rows
+	all_winning_pos.append(np.uint32(0b11111 << (x*5)))
+	#cols
+	all_winning_pos.append(np.uint32(0b0000100001000010000100001 << x))
+
+# 01111
+# 10111
+# 11011
+# 11101
+# 11110
+all_winning_pos.append(np.uint32(0b0111110111110111110111110))
+# 11110
+# 11101
+# 11011
+# 10111
+# 01111
+all_winning_pos.append(np.uint32(0b1111011101110111011101111))
+
+# for board in all_winning_pos:
+# 	print(np.binary_repr(board))
+
+numbers = []
+boards = []
+board_map = {}
+
+with open(args.input, "r") as p_input:
 	for line in p_input:
-		chars = [c for c in line.strip()]
-		all_numbers.append(np.array([int(c) for c in chars]))
+		line = line.strip()
+		# read input numbers from first line
+		if len(numbers) == 0:
+			numbers = [int(x) for x in line.split(",")]
+			continue
 
-all_numbers = np.array(all_numbers)
-num_bits = all_numbers.shape[1]
+		# empty line -> a new board
+		if len(line) == 0:
+			boards.append([])
+			continue
 
-def calc(numbers, filter_func):
-	bit_pos = 0
-	while len(numbers) > 1:
-		bits = numbers[:, bit_pos]
-		ones = np.sum(bits)
-		zeros = np.sum(vectorized_invert(bits))
-		numbers = numbers[np.where(numbers[:, bit_pos] == filter_func(ones, zeros))]
-		bit_pos += 1
+		boards[-1] += [int(x) for x in line.split()]
 
-	return reduce(lambda a, b: ((a << 1) | b), numbers[0])
+for i, board in enumerate(boards):
+	print(board)
+	for j, x in enumerate(board):
+		if x in board_map:
+			board_map[x].append((i, j))
+		else:
+			board_map[x] = [(i, j)]
 
-print(calc(all_numbers, lambda ones, zeros: ones >= zeros) * calc(all_numbers, lambda ones, zeros: ones < zeros))
+boards = np.array(boards, dtype=np.uint32)
+board_mask = np.zeros((len(boards)), dtype=np.uint32)
+
+for x in numbers:
+	print(x)
+	if x not in board_map:
+		print("skipped", x, "not in any board")
+		continue
+	for i, j in board_map[x]:
+		print (i, j)
+		board_mask[i] |= np.uint32(1 << j)
+
+	for winning_pos in all_winning_pos:
+		for board, mask in zip(boards, board_mask):
+			if mask & winning_pos == winning_pos:
+				print("We have a winner!", board, mask)
+				unpacker = 2**np.arange(25, dtype=mask.dtype)
+				unpacked = (~(mask & unpacker).astype(bool)).astype(np.uint32)
+				print(np.sum(unpacked * board) * x)
+				exit(0)
+	print()
+
