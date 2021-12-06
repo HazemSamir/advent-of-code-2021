7e0c32c002d6cbf3ce561cc1c1856fa62bd7ed8f
Hazem Ash <zashm@pm.me>
2021-12-06 07:45:42 +0000

    Solve day#6 part#2

--- advent-of-code.py
+++ advent-of-code.py
@@ -1,9 +1,9 @@
-# Solution for https://adventofcode.com/2021/day/5
+# Solution for https://adventofcode.com/2021/day/6
 
 import argparse
 import numpy as np
 
-parser = argparse.ArgumentParser(description='Solve advent-of-code-2021 day#6 part#1')
+parser = argparse.ArgumentParser(description='Solve advent-of-code-2021 day#6 part#2')
 parser.add_argument('-input', '-i', metavar='input_file', type=str, required=True,
                     help='file with puzzle input')
 
@@ -17,12 +17,15 @@ with open(args.input, "r") as p_input:
 	for line in p_input:
 		state = [int(x) for x in line.strip().split(",")]
 
-NUM_DAYS = 80
-
+NUM_DAYS = 256
+dp = {}
 def generate(x):
+	if x in dp:
+		return dp[x]
 	if x > NUM_DAYS:
 		return 0
-	return 1 + generate(x + 7) + generate(x + 9)
+	dp[x] = 1 + generate(x + 7) + generate(x + 9)
+	return dp[x]
 
 s = len(state)
 for x in state:
