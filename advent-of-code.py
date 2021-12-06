0929bc69d23b9c10bcf04a259037363738af4f4b
Hazem Ash <zashm@pm.me>
2021-12-06 07:45:10 +0000

    Solve day#6 part#1

--- advent-of-code.py
+++ advent-of-code.py
@@ -3,7 +3,7 @@
 import argparse
 import numpy as np
 
-parser = argparse.ArgumentParser(description='Solve advent-of-code-2021 day#5 part#2')
+parser = argparse.ArgumentParser(description='Solve advent-of-code-2021 day#6 part#1')
 parser.add_argument('-input', '-i', metavar='input_file', type=str, required=True,
                     help='file with puzzle input')
 
@@ -11,30 +11,22 @@ args = parser.parse_args()
 print(args.input)
 
 ########################### Solution ############################
-vents = []
-dims = [0, 0]
+state = []
 
 with open(args.input, "r") as p_input:
 	for line in p_input:
-		line = [p.split(",") for p in line.strip().split(" -> ")]
-		vents.append([[int(c[0]), int(c[1])] for c in line])
-		dims = [max([dims[0]] + [vents[-1][i][0] + 1 for i in range(2)]),
-				max([dims[1]] + [vents[-1][i][1] + 1 for i in range(2)])]
+		state = [int(x) for x in line.strip().split(",")]
 
-m = np.zeros(dims, dtype=np.int32)
-for b, e in vents:
-	if b[0] == e[0] or b[1] == e[1]:
-		fixed_b = [min(b[0], e[0]), min(b[1], e[1])]
-		fixed_e = [max(b[0], e[0]) + 1, max(b[1], e[1]) + 1]
-		m[fixed_b[0] : fixed_e[0], fixed_b[1] : fixed_e[1]] += 1
-	else:
-		x_step = 1 if b[0] < e[0] else -1
-		y_step = 1 if b[1] < e[1] else -1
-		m[np.arange(b[0], e[0] + x_step, x_step), np.arange(b[1], e[1] + y_step, y_step)] += 1
+NUM_DAYS = 80
+
+def generate(x):
+	if x > NUM_DAYS:
+		return 0
+	return 1 + generate(x + 7) + generate(x + 9)
+
+s = len(state)
+for x in state:
+	s += generate(x+1)
+print(s)
 
 
-print(m)
-# converting first bit to 0 only keeps values > 1, converts 1 to zeros.
-# we don't actually care about other values.
-m &= (~np.int32(0) << 1)
-print(np.count_nonzero(m))
