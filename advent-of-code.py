03c000e18c1f7f31eb5f84f0f3b9e3dcf7455eb3
Hazem Ash <zashm@pm.me>
2021-12-08 10:23:12 +0000

    Solve day#8 part#1

--- advent-of-code.py
+++ advent-of-code.py
@@ -1,9 +1,9 @@
-# Solution for https://adventofcode.com/2021/day/7
+# Solution for https://adventofcode.com/2021/day/8
 
 import argparse
 import numpy as np
 
-parser = argparse.ArgumentParser(description='Solve advent-of-code-2021 day#7 part#2')
+parser = argparse.ArgumentParser(description='Solve advent-of-code-2021 day#8 part#1')
 parser.add_argument('-input', '-i', metavar='input_file', type=str, required=True,
                     help='file with puzzle input')
 
@@ -15,27 +15,12 @@ state = []
 
 with open(args.input, "r") as p_input:
 	for line in p_input:
-		state = [int(x) for x in line.strip().split(",")]
+		line = line.strip().split("|")
+		state.append([x.split() for x in line])
 
-
-lo = min(state)
-hi = max(state)
-
-def CalcCost(p):
-	cost = 0
-	for st in state:
-		diff = abs(p - st)
-		cost += (diff * (diff + 1)) / 2
-	return cost
-
-while lo < hi:
-	mid = int((lo+hi) / 2)
-	if mid == lo:
-		break
-
-	if CalcCost(mid) > CalcCost(mid+1):
-		lo = mid
-	else:
-		hi = mid
-
-print(min(CalcCost(lo), CalcCost(hi)))
+count = 0
+for x in state:
+	for output in x[1]:
+		if len(output) in [2, 3, 4, 7]:
+			count += 1
+print(count)
