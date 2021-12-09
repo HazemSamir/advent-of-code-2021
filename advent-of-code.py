1701f0cbaf8da924c6c5e5559a8a286679a31977
Hazem Ash <zashm@pm.me>
2021-12-09 01:12:28 +0000

    Solve day#8 part#2

--- advent-of-code.py
+++ advent-of-code.py
@@ -2,8 +2,10 @@
 
 import argparse
 import numpy as np
+from itertools import permutations
 
-parser = argparse.ArgumentParser(description='Solve advent-of-code-2021 day#8 part#1')
+
+parser = argparse.ArgumentParser(description='Solve advent-of-code-2021 day#8 part#2')
 parser.add_argument('-input', '-i', metavar='input_file', type=str, required=True,
                     help='file with puzzle input')
 
@@ -18,9 +20,40 @@ with open(args.input, "r") as p_input:
 		line = line.strip().split("|")
 		state.append([x.split() for x in line])
 
+all_perms = [''.join(p) for p in permutations('abcdefg')]
+
+possible_numbers = []
+for p in all_perms:
+	numbers = 10 * [None]
+	#             a      b      c      d      e      f      g
+	numbers[0] = p[0] + p[1] + p[2] +        p[4] + p[5] + p[6]
+	numbers[1] =               p[2]               + p[5]
+	numbers[2] = p[0]        + p[2] + p[3] + p[4]        + p[6]
+	numbers[3] = p[0]        + p[2] + p[3]        + p[5] + p[6]
+	numbers[4] =        p[1] + p[2] + p[3]        + p[5]
+	numbers[5] = p[0] + p[1]        + p[3]        + p[5] + p[6]
+	numbers[6] = p[0] + p[1]        + p[3] + p[4] + p[5] + p[6]
+	numbers[7] = p[0]        + p[2]               + p[5]
+	numbers[8] = p[0] + p[1] + p[2] + p[3] + p[4] + p[5] + p[6]
+	numbers[9] = p[0] + p[1] + p[2] + p[3]        + p[5] + p[6]
+	possible_numbers.append([''.join(sorted(n)) for n in numbers])
+
 count = 0
-for x in state:
-	for output in x[1]:
-		if len(output) in [2, 3, 4, 7]:
-			count += 1
+for st in state:
+	st_number = ''
+	st[0] = [''.join(sorted(s)) for s in st[0]]
+	st[1] = [''.join(sorted(s)) for s in st[1]]
+	for n in possible_numbers:
+		found_all = True
+		for x in st[0] + st[1]:
+			if x not in n:
+				# print(x, "not in", n)
+				found_all = False
+				break
+		if found_all:
+			for output in st[1]:
+				st_number += str(n.index(output))
+			break
+	count += int(st_number)
+	# print(count, st_number)
 print(count)
