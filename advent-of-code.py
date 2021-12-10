09908731a06231a6deaaa95e80fe90ed1b4b8980
Hazem Ash <zashm@pm.me>
2021-12-10 10:57:04 +0000

    Solve day#9 part#2

--- advent-of-code.py
+++ advent-of-code.py
@@ -5,7 +5,7 @@ import numpy as np
 from itertools import permutations
 
 
-parser = argparse.ArgumentParser(description='Solve advent-of-code-2021 day#9 part#1')
+parser = argparse.ArgumentParser(description='Solve advent-of-code-2021 day#9 part#2')
 parser.add_argument('-input', '-i', metavar='input_file', type=str, required=True,
                     help='file with puzzle input')
 
@@ -22,8 +22,23 @@ with open(args.input, "r") as p_input:
 h = len(hmap)
 w = len(hmap[0])
 
-s = 0
+vis = [[False for y in range(w)] for x in range(h)]
+
+def dfs_basin(x, y):
+	if x < 0 or y < 0 or x >= h or y >= w:
+		return 0
+	if vis[x][y]:
+		return 0
+	vis[x][y] = True
+	if hmap[x][y] == 9:
+		return 0
+	return 1 + dfs_basin(x + 1, y) + dfs_basin(x - 1, y) + dfs_basin(x, y + 1) + dfs_basin(x, y - 1)
+
+basins = []
 for i in range(h):
 	for j in range(w):
-		s += (hmap[i][j] + 1) if (i - 1 < 0 or hmap[i][j] < hmap[i - 1][j]) and (j - 1 < 0 or hmap[i][j] < hmap[i][j - 1]) and (i + 1 >= h or hmap[i][j] < hmap[i + 1][j]) and (j + 1 >= w or hmap[i][j] < hmap[i][j + 1]) else 0
-print(s)
+		if not vis[i][j] and hmap[i][j] != 9:
+			basins.append(dfs_basin(i, j))
+
+basins = sorted(basins)
+print(basins[-1] * basins[-2] * basins[-3])
