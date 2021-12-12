2ee722ce64cbcf2acfec90f6ef709819e158d1ef
Hazem Ash <zashm@pm.me>
2021-12-12 08:43:27 +0000

    Solve day#12 part#1

--- advent-of-code.py
+++ advent-of-code.py
@@ -1,10 +1,10 @@
-# Solution for https://adventofcode.com/2021/day/11
+# Solution for https://adventofcode.com/2021/day/12
 
 import argparse
 import numpy as np
 
 
-parser = argparse.ArgumentParser(description='Solve advent-of-code-2021 day#11 part#2')
+parser = argparse.ArgumentParser(description='Solve advent-of-code-2021 day#12 part#1')
 parser.add_argument('-input', '-i', metavar='input_file', type=str, required=True,
                     help='file with puzzle input')
 
@@ -13,51 +13,37 @@ print(args.input)
 
 ########################### Solution ############################
 def ReadInput():
-	inpt = []
+	inpt = {}
 	with open(args.input, "r") as p_input:
 		for line in p_input:
-			inpt.append([int(c) for c in line.strip()])
+			s, e = line.strip().split("-")
+			if e != 'start' and s != 'end':
+				AddToAdjecentList(s, e, inpt)
+			if s != 'start' and e != 'end':
+				AddToAdjecentList(e, s, inpt)
 
-	return np.array(inpt, dtype=np.uint8)
+	return inpt
 
-def Adjecent(i, j):
-	return [(i + x, j + y) for x in range(-1, 2) for y in range(-1, 2)]
+def AddToAdjecentList(s, e, adj_list):
+	if s not in adj_list:
+		adj_list[s] = []
+	adj_list[s].append(e)
 
-def Flash(i, j, octmap, flashed):
-	if i < 0 or j < 0 or i >= octmap.shape[0] or j >= octmap.shape[1] or flashed[i][j]:
+
+def dfs(c, cave_map, visited):
+	if c in visited:
 		return 0
-	if octmap[i][j] <= 9:
-		octmap[i][j] += 1
-		return 0
+	if c == 'end':
+		return 1
+	if c.islower():
+		visited.append(c)
+	count = 0
+	for adj in cave_map[c]:
+		count += dfs(adj, cave_map, visited)
+	if c.islower():
+		visited.pop()
+	return count
 
-	flashed[i][j] = True
-	octmap[i][j] = 0
-	s = 1
-	for x, y in Adjecent(i, j):
-		s += Flash(x, y, octmap, flashed)
-	return s
+cave_map = ReadInput()
+print(dfs('start', cave_map, []))
 
-def FlashAll(octmap, flashed):
-	flash_count = 0
-	for i, j in np.transpose((octmap > 9).nonzero()):
-		flash_count += Flash(i, j, octmap, flashed)
-	return flash_count
-
-def Step(octmap):
-	octmap += np.uint8(1)
-	flashed = np.zeros(octmap.shape, dtype=bool)
-	all_flash_count = 0
-	flash_count = FlashAll(octmap, flashed)
-	while flash_count > 0:
-		all_flash_count += flash_count
-		flash_count = FlashAll(octmap, flashed)
-
-	return all_flash_count
-
-octmap = ReadInput()
-step_count = 0
-while np.count_nonzero(octmap) > 0:
-	Step(octmap)
-	step_count += 1
-
-print(step_count)
